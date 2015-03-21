9674a93ff6a9e1e7c0f75144e83aa569b4f4cd8d
Kristian Holm <kristian.crash.holm@gmail.com>
2015-03-21 21:08:03 +0100

    make the minimal config more minimal

--- kristian-bindings.el
+++ kristian-bindings.el
@@ -15,9 +15,9 @@
 
 ;; Makes ctrl+tab cycle through the buffers (the different files) you
 ;; have open. Very handy if you're not quite sure what your looking for.
-(global-set-key (kbd "<C-tab>") 'bury-buffer)
+;(global-set-key (kbd "<C-tab>") 'bury-buffer)
 
 ;; let me have access to an eshell
-(global-set-key (kbd "C-c m") 'eshell)
+;(global-set-key (kbd "C-c m") 'eshell)
 
 (provide 'kristian-bindings)
--- kristian.el
+++ kristian.el
@@ -23,6 +23,24 @@
 
 ;;;; Load my user (kristian) specific elisp-files
 ;; load my keybindings
-(require 'kristian-bindings)
+;;(require 'kristian-bindings)
 ;; load dired-settings
-(require 'kristian-dired-settings)
+;;(require 'kristian-dired-settings)
+
+
+;;;; Keybindings
+;;; kristian-bindings.el --- let's me tame the mighty beast with keystrokes
+
+;; instead of using the alt-key, which is not as easy to reach 
+;; from the home row, use ctrl+x and ctrl+m
+(global-set-key "\C-x\C-m" 'execute-extended-command)
+(global-set-key "\C-c\C-m" 'execute-extended-command)
+
+;; If you misspell a word, insted of tapping backspace, simply
+;; use ctrl+w to delete the whole word and write it again. In my case 
+;; this way is faster then using backspace to delete one character 
+;; after another.
+(global-set-key "\C-w" 'backward-kill-word)
+(global-set-key "\C-c\C-k" 'kill-region)
+(global-set-key "\C-x\C-k" 'kill-region)
+
